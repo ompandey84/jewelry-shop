30094fe26700b3bf45a1d3fe02d8758622b60ccb
ompandey84 <ompandey8435@gmail.com>
2026-02-17 17:16:41 +0530

    Fix static files: Correct STATIC_ROOT path for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,3 @@
 python3 -m pip install -r requirements.txt --break-system-packages
+mkdir -p staticfiles_build/static
 python3 manage.py collectstatic --noinput --clear
--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
